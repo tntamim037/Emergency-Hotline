c836bfc8bc6907caa7458970657d5b887e14075d
tntamim037 <tntamim037n@gmail.com>
2025-08-27 18:33:04 +0600

    fourth

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log("");
+function increaseLove(){
+    const loveElement=document.getElementById("love_count")
+    let currentCount=parseInt(loveElement.innerText)
+    currentCount +=1
+    loveElement.innerText = currentCount;
+}
