ad9bbd9ef358352b149d492754b367b89834f437
tntamim037 <tntamim037n@gmail.com>
2025-08-29 01:06:23 +0600

    'nineth'

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//love count
+//love count...
 function increaseLove(){
     const loveElement=document.getElementById("love_count")
     let currentCount=parseInt(loveElement.innerText)
@@ -6,9 +6,61 @@ function increaseLove(){
     loveElement.innerText = currentCount;
 }
 
+// call button all...
 
+let coins=100;
 
+//sobaik dhoro...
 
+const callButtons=document.getElementsByClassName("call_btn");
+const clearButton=document.getElementById("clean_history") ;
+const callHistoryList=document.getElementById("call_history_list");
+const coinDisplay=document.getElementById("coin_count")
+
+//loop is on....
+
+for(const btn of callButtons){
+    btn.addEventListener("click",function(){
+        const serviceName=btn.parentNode.parentNode.children[2].innerText
+        const serviceName2=btn.parentNode.parentNode.children[1].innerText
+        // console.log(serviceName);
+        const serviceNumber=btn.parentNode.parentNode.children[3].innerText
+        // console.log(serviceNumber);
+
+//check.....
+
+        if(coins<20){
+            alert("❌ আপনার পর্যান্ত কয়েন নেই। কল করতে কমপক্ষে ২০ কয়েন লাগবে।");
+            return ;
+        }
+        coins -=20;
+        coinDisplay.innerText=coins ;
+        
+        alert(`📞 calling ${serviceName} ${serviceNumber}....`);
+
+        const now=new Date();
+        const myTime=now.toLocaleTimeString();
+
+        const li=document.createElement("li");
+        li.className="flex justify-between items-center py-1";
+        const leftDiv=document.createElement("div");
+        leftDiv.innerHTML=`<p class="font-semibold">${serviceName2}</p>
+                    <p class="text-sm text-gray-500">${serviceNumber}</p>`
+        const rightDiv=document.createElement("div");
+        rightDiv.innerText=myTime ;
+        rightDiv.className="text-sm"
+       li.appendChild(leftDiv)
+       li.appendChild(rightDiv)
+        callHistoryList.appendChild(li);
+
+    })
+}
+
+//clear history
+
+clearButton.addEventListener("click",function(){
+    callHistoryList.innerHTML="";
+})
 
 
 
